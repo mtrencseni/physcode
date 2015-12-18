c2ac15447f4e2827ba9367bcc3b84c74208a6f77
mtrencseni <mtrencseni@gmail.com>
2015-12-18 17:02:54 +0000

    Cosmetic.

--- cpp/PistonGas.cpp
+++ cpp/PistonGas.cpp
@@ -2,7 +2,7 @@
 #include <assert.h>
 #include "Quantity.h"
 
-/* === Simple thermodynamics === */
+/* === Piston + gas thermodynamics === */
 
 typedef Quantity< 1,  0,  0, 0,  0, 0,  0, double> Length;
 typedef Quantity< 2,  0,  0, 0,  0, 0,  0, double> Area;
@@ -20,43 +20,43 @@ typedef Quantity< 0,  1,  0, 0,  0, 0, -1, double> MolarMass;
 typedef Quantity< 2,  1, -2, 0, -1, 0, -1, double> GasConstant;
 
 struct Configuration {
-    Pressure    P; // pressure of the gas
-    Length      h; // height of the gas
-    Velocity    velocity; // velocity of the piston
+    Pressure    gasPressure;    // pressure of the gas
+    Length      gasHeight;      // height of the gas
+    Velocity    pistonVelocity; // velocity of the piston
 };
 
 struct Parameters {
-    Area        A; // area of the piston and gas chamber
-    Temperature T; // isotherm temperature of the gas
-    Mass        pistonMass; // mass of the piston
-    Mass        gasMass; // mass of the gas
-    MolarMass   gasMolarMass; // molar mass of the gas
-    GasConstant R; // ideal gas constant
+    Area        pistonArea;     // area of the piston and gas chamber
+    Temperature gasTemperature; // isotherm temperature of the gas
+    Mass        pistonMass;     // mass of the piston
+    Mass        gasMass;        // mass of the gas
+    MolarMass   gasMolarMass;   // molar mass of the gas
+    GasConstant R;              // ideal gas constant
 };
 
 void solveForIdealGasPressure(const Parameters& p, Configuration& c)
 {
     Moles  n = p.gasMass / p.gasMolarMass;
-    Volume V = p.A * c.h;
-    c.P = n * p.R * p.T / V; // PV = nRT
+    Volume gasVolume = p.pistonArea * c.gasHeight;
+    c.gasPressure = n * p.R * p.gasTemperature / gasVolume; // PV = nRT
 }
 
 Acceleration acceleration(const Parameters& p, const Configuration& c)
 {
     Acceleration g  = -9.8 * meter / (second * second);
     Force fGravity  = p.pistonMass * g;
-    Force fPressure = c.P * p.A;
+    Force fPressure = c.gasPressure * p.pistonArea;
     Force fTotal    = fGravity + fPressure;
     Acceleration a  = fTotal / p.pistonMass;
-    std::cout << a.str() << std::endl;
+    //std::cout << a.str() << std::endl;
     return a;
 }
 
 Configuration step(const Parameters& p, const Configuration& c, const Length& dx, const Time& dt)
 {
     Configuration cp = c;
-    cp.h   = c.h + c.velocity * dt;
-    cp.velocity = c.velocity + acceleration(p, cp) * dt;
+    cp.gasHeight   = c.gasHeight + c.pistonVelocity * dt;
+    cp.pistonVelocity = c.pistonVelocity + acceleration(p, cp) * dt;
     solveForIdealGasPressure(p, cp);
     return cp;
 }
@@ -79,12 +79,11 @@ int main()
     
     solveForIdealGasPressure(p, c);
 
-    for (unsigned i = 0; i < 1000; i++)
+    for (unsigned i = 0; i < 1000*1000; i++)
     {
         c = step(p, c, dx, dt);
-        std::cout << c.h.str() << std::endl;
+        std::cout << c.gasHeight.val << std::endl;
     }
     
     return 0;
 }
-
